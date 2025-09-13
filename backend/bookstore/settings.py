7c36f2ff885e1f7f172806483e0e45ce62db12d1
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-09-13 12:16:25 +0530

    Update settings.py

--- backend/bookstore/settings.py
+++ backend/bookstore/settings.py
@@ -140,6 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # if you have project-level static folder
 ]
+STATICFILES_DIRS = [BASE_DIR / "backend/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
